f645246d2187e00963922d2acf8b6682b0f74063
wjdrl3122 <wjdrl3122@gmail.com>
2023-03-06 21:06:19 +0900

    [Feat] 파티 API 업데이트 중

--- src/modules/party/party.controller.ts
+++ src/modules/party/party.controller.ts
@@ -1,37 +1,85 @@
-import { Controller, Get, Patch, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    InternalServerErrorException,
+    NotFoundException,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
 import { PartyService } from './party.service';
 import { CreatePartyDto } from './dto/create-party.dto';
 import { UpdatePartyDto } from './dto/update-party.dto';
 
-@Controller('party')
+@Controller('api/party')
 export class PartyController {
     constructor(private readonly partyService: PartyService) {}
 
-// // 파티 목록 조회
-// @Get('/party-list')
+    // 파티 목록 조회
+    @Get('/list')
+    async getParties() {
+        return await this.partyService.getParties();
+    }
 
-// //파티 상세 조회
-// @Get('/party/:partyId')
+    //파티 상세 조회
+    @Get(':partyId')
+    async getPartyById(@Param('partyId') partyId: number) {
+        return await this.partyService.getPartyById(partyId);
+    }
 
-// // 파티 등록
-// @Post('/party')
+    // 파티 등록
+    @Post()
+    async createParty(@Body() party: CreatePartyDto) {
+        return await this.partyService.createParty(party);
+    }
 
-// // 파티 수정
-// @Patch('/party/:partyId')
+    // 파티 수정
+    @Patch(':partyId')
+    async updateParty(@Param('partyId') partyId: number, @Body() party: UpdatePartyDto) {
+        try {
+            // const { id } = req.user
+            const userId = 1;
+            await this.partyService.updateParty(userId, partyId, party);
+            return { success: true };
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw new NotFoundException(error.message);
+            } else if (error instanceof InternalServerErrorException) {
+                throw new InternalServerErrorException(error.message);
+            } else {
+                throw error;
+            }
+        }
+    }
 
-// // 파티 삭제
-// @Delete('/party/:partyId')
+    // // 파티 삭제
+    // @Delete(':partyId')
 
-// // 파티 신청
-// @Post('/party/apply/:partyId')
+    // 파티 신청
+    @Post('apply/:partyId')
+    async applyParty(@Param('partyId') partyId: number) {
+        try {
+            let userId = Number(1);
+            return await this.partyService.applyParty(partyId, userId);
+        } catch (error) {
+            throw new InternalServerErrorException(error.message);
+        }
+    }
 
-// // 파티 신청 취소
-// @Delete('/party/apply-cancle/:partyId')
+    // 파티 신청자 목록 조회
+    @Get('/:partyId/members')
+    async getPartyMembers(@Param('partyId') partyId: number) {
+        try {
+            return await this.partyService.getPartyMembers(partyId);
+        } catch (error) {
+            throw new InternalServerErrorException(error.message);
+        }
+    }
 
-// // 파티 신청자 목록 조회
-// @Get('/party/:partyId/members')
+    // // 파티 신청 취소
+    // @Delete('/party/apply-cancle/:partyId')
 
-// // 파티 승낙/ 거절
-// @Patch('/party/:partyId/members/:userId')
-
+    // // 파티 승낙/ 거절
+    // @Patch('/party/:partyId/members/:userId')
 }
--- src/modules/party/party.service.ts
+++ src/modules/party/party.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreatePartyDto } from './dto/create-party.dto';
+import { UpdatePartyDto } from './dto/update-party.dto';
 import { PartyMember } from './entity/party-member.entity';
 import { Party } from './entity/party.entity';
 import { Thumbnail } from './entity/thumbnail.entity';
@@ -11,7 +13,68 @@ export class PartyService {
         @InjectRepository(Party) private partyRepository: Repository<Party>,
         @InjectRepository(Thumbnail) private ThumbnailRepository: Repository<Thumbnail>,
         @InjectRepository(PartyMember) private PartyMembersRepository: Repository<PartyMember>,
-      ) {}
+    ) {}
 
-      
+    async getParties() {
+        return await this.partyRepository.find({
+            where: { deletedAt: null },
+        });
+    }
+
+    async getPartyById(partyId: number) {
+        return await this.partyRepository.findOne({
+            where: { id: partyId, deletedAt: null },
+        });
+    }
+
+    async createParty(party: CreatePartyDto) {
+        return await this.partyRepository.insert(party);
+    }
+
+    async updateParty(
+        userId: number,
+        partyId: number,
+        party: UpdatePartyDto,
+    ): Promise<void | InternalServerErrorException> {
+        try {
+            const result = await this.partyRepository.update(partyId, party);
+            if (result.affected === 0) {
+                throw new NotFoundException(`파티 아이디 중 ${partyId} 를 찾을 수 없습니다.`);
+            }
+        } catch (error) {
+            throw new InternalServerErrorException(
+                `파티 업데이트에 실패했습니다 : ${error.message}`,
+            );
+        }
+    }
+
+    async applyParty(partyId: number, userId: number) {
+        try {
+            const party = await this.partyRepository.findOne({
+                where: { id: partyId, deletedAt: null },
+            });
+            if (!party) {
+                throw new NotFoundException('파티를 찾을 수 없습니다');
+            }
+
+            const result = await this.PartyMembersRepository.insert({
+                partyId,
+                userId,
+            });
+        } catch (error) {
+            throw new InternalServerErrorException(`파티 신청에 실패했습니다 : ${error.message}`);
+        }
+    }
+
+    async getPartyMembers(partyId: number) {
+        try {
+            return await this.PartyMembersRepository.find({
+                where: { partyId },
+            });
+        } catch (error) {
+            throw new InternalServerErrorException(
+                `파티맴버 조회를 실패했습니다 : ${error.message}`,
+            );
+        }
+    }
 }
