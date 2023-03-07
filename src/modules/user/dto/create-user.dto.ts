cd3681ab5e24d11e4993e943f972b6acae7c34be
Younghwan90s <younghwan1608@gmail.com>
2023-03-07 21:02:06 +0900

    [Feat] auth 기능 스크립트 추가

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,44 +1,41 @@
 import { IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
+    @IsString()
+    readonly email: string;
 
-  @IsString()
-  readonly email: string;
+    @IsString()
+    readonly password: string;
 
-  @IsString()
-  readonly password: string;
+    @IsString()
+    readonly confirmPassword: string;
 
-  @IsString()
-  readonly confirmPassword: string;
+    @IsString()
+    readonly name: string;
 
-  @IsString()
-  readonly name: string;
+    @IsString()
+    readonly sex: string;
 
-  @IsString()
-  readonly sex: string;
+    @IsString()
+    readonly phone: string;
 
-  @IsString()
-  readonly phone: string;
+    @IsOptional()
+    @IsString()
+    readonly birthday: string;
 
-  @IsOptional()
-  @IsString()
-  readonly birthday: string;
+    @IsOptional()
+    @IsString()
+    readonly region: string;
 
-  @IsOptional()
-  @IsString()
-  readonly region: string;
+    @IsOptional()
+    @IsString()
+    readonly address: string;
 
-  @IsOptional()
-  @IsString()
-  readonly address: string;
+    @IsOptional()
+    @IsString()
+    readonly profile: string;
 
-  @IsOptional()
-  @IsString()
-  readonly profile: string;
-
-  @IsOptional()
-  @IsString()
-  readonly introduction: string;
-
+    @IsOptional()
+    @IsString()
+    readonly introduction: string;
 }
-
