ac67789ddbecd046c81c551c56a98a4aef5c1584
Younghwan90s <younghwan1608@gmail.com>
2023-03-14 17:16:32 +0900

    [Refactor] review 및 auth api  타입 명시 및 코드 수정

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,6 +1,11 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { Review } from './entity/review.entity';
 
@@ -8,17 +13,17 @@ import { Review } from './entity/review.entity';
 export class ReviewService {
     constructor(@InjectRepository(Review) private reviewRepository: Repository<Review>) {}
 
-    async writeReview(userId: number, partyId: number, data: CreateReviewDto):Promise<Review> {
-        const review = new Review()
-        review.userId = userId
-        review.partyId = partyId
-        review.rating = data.rating
-        review.review = data.review
+    async writeReview(userId: number, partyId: number, data: CreateReviewDto): Promise<Review> {
+        const review = new Review();
+        review.userId = userId;
+        review.partyId = partyId;
+        review.rating = data.rating;
+        review.review = data.review;
 
-       return await this.reviewRepository.save(review)
+        return await this.reviewRepository.save(review);
     }
 
-    async readReview(partyId: number): Promise<any> {
+    async readReview(partyId: number): Promise<Review[]> {
         const reviews = await this.reviewRepository.find({
             where: { partyId, deletedAt: null },
             order: { createdAt: 'DESC' },
@@ -30,31 +35,19 @@ export class ReviewService {
         return reviews;
     }
 
-    async updateReview(reviewId: number, rating: string, review: string) {
-        const updatedReview = { rating, review };
-        const result = await this.reviewRepository.update(reviewId, updatedReview);
+    async updateReview(reviewId: number, rating: string, review: string): Promise<UpdateResult> {
+        return await this.reviewRepository.update(reviewId, { rating, review });
     }
 
-    async deleteReview(id: number): Promise<DeleteResult> {
-        const review = await this.reviewRepository.findOne({ where: { id } });
+    async deleteReview(id, reveiwId: number): Promise<DeleteResult> {
+        const review = await this.reviewRepository.findOne({ where: { id: reveiwId } });
         if (!review) {
             throw new NotFoundException('리뷰가 없습니다.');
-        } else {
-            return await this.reviewRepository.softDelete(review.id);
         }
-    }
+        if (review.userId !== id) {
+            throw new UnauthorizedException('회원 본인만 삭제할 수 있습니다.');
+        }
 
-    async getReviewAdmin() {
-        const reviews = await this.reviewRepository.find({
-            relations: ['party', 'user'],
-            order: { partyId: 'DESC' },
-            withDeleted: true,
-        });
-        console.log(reviews);
-        return reviews;
-    }
-
-    async deleteReviewAdmin(id: number) {
-        return await this.reviewRepository.softDelete(id);
+        return await this.reviewRepository.softDelete(review.id);
     }
 }
